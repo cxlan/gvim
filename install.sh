c8f6b21955ff424de9aea00af94eba9c4af83ce2
cxlan <git9999@163.com>
2020-04-15 12:00:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp .vimrc ~/.vimrc
-mkdir ~/.vim/bundle && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+mkdir -p　 ~/.vim/bundle && git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo 'install success!'
